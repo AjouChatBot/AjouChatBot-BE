cd9a597bf74ef2f4263adb027710c0062154b1d1
Soyoung Yang <ysbeam@naver.com>
2025-03-08 12:22:11 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/io/saim/AjouChatBot_BE/AjouChatBotBeApplication.java
@@ -0,0 +1,13 @@
+package io.saim.AjouChatBot_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouChatBotBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjouChatBotBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/saim/AjouChatBot_BE/AjouChatBotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.saim.AjouChatBot_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjouChatBotBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
