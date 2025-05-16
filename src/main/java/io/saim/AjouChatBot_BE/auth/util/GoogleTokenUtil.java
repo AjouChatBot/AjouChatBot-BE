6c077ee4b4b910d7dd53d1df5ecac1f4ade5d67c
Soyoung Yang <ysbeam@naver.com>
2025-05-17 06:35:54 +0900

    refactor: google.client-id 구현 방식 변경

--- src/main/java/io/saim/AjouChatBot_BE/auth/util/GoogleTokenUtil.java
+++ src/main/java/io/saim/AjouChatBot_BE/auth/util/GoogleTokenUtil.java
@@ -2,41 +2,36 @@ package io.saim.AjouChatBot_BE.auth.util;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.stereotype.Component;
-import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
 import java.util.Collections;
-import java.util.Properties;
 
 @Component
 public class GoogleTokenUtil {
 
 	private final String clientId;
 
-	public GoogleTokenUtil() {
-		try (InputStream input = getClass().getClassLoader().getResourceAsStream(".env")) {
-			Properties prop = new Properties();
-			prop.load(input);
-			this.clientId = prop.getProperty("GOOGLE_CLIENT_ID");
-		} catch (Exception e) {
-			throw new RuntimeException(".env에서 GOOGLE_CLIENT_ID를 불러올 수 없습니다.", e);
-		}
+	public GoogleTokenUtil(@Value("${google.client-id}") String clientId) {
+		this.clientId = clientId;
 	}
+
 	public GoogleIdToken.Payload verify(String idTokenString) {
 		try {
 			GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
-				new NetHttpTransport(),
-				JacksonFactory.getDefaultInstance())
+				GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance()
+			)
 				.setAudience(Collections.singletonList(clientId))
 				.build();
 
 			GoogleIdToken idToken = verifier.verify(idTokenString);
-			return (idToken != null) ? idToken.getPayload() : null;
-
+			if (idToken != null) {
+				return idToken.getPayload();
+			}
 		} catch (Exception e) {
-			e.printStackTrace(); //로그 확인용
-			return null;
+			e.printStackTrace();
 		}
+		return null;
 	}
 }
